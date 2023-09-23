891acaa28a8c567c0a7e122319a9e6558783e630
Anubhab Chowdhury <99398916+Anubhab2003@users.noreply.github.com>
2023-09-23 12:34:29 +0000

    Operations on linked list

--- LinkedList.java
+++ LinkedList.java
@@ -1,51 +1,67 @@
 import java.util.*;
-public class LinkedList{
-    public static class Node{
+
+public class LinkedList {
+    public static class Node {
         int data;
         Node next;
-        public Node(int data){
-            this.data=data;
-            this.next=null;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
         }
     }
+
     public static Node head;
     public static Node tail;
-    public void addFirst(int data){
-        Node newNode= new Node(data);
-        if(head==null){
-            head=tail=newNode;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
             return;
         }
-        newNode.next=head;
-        head=newNode;
+        newNode.next = head;
+        head = newNode;
     }
-    public void addLast(int data){
-        Node newNode=new Node(data);
-        if(head==null){
-            head=tail=newNode;
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
             return;
         }
-        tail.next=newNode;
-        tail=newNode;
+        tail.next = newNode;
+        tail = newNode;
     }
-    public static void print(){
-        Node temp=head;
-        while(temp!=null){
-            System.out.print(temp.data+"->");
-            temp=temp.next;
+
+    public static void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
         }
         System.out.println("null");
     }
-    public static void main(String args[]){
-        LinkedList ll=new LinkedList();
-        ll.addFirst(2);
-        ll.print();
-        ll.addFirst(1);
-        ll.print();
-        ll.addLast(3);
-        ll.print();
-        ll.addLast(4);
-        ll.print();
+
+    public static void add(int idx, int data) {
+        Node newNode = new Node(data);
+        Node temp = head;
+        int i = 0;
+        while (i < idx - 1) {
+            temp = temp.next;
+            i++;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
     }
 
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.add(2, 120);
+        ll.print();
+    }
 }

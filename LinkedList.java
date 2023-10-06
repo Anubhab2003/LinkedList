d5f65a3250acf704c63431dcd587e12d14c13f94
Anubhab Chowdhury <99398916+Anubhab2003@users.noreply.github.com>
2023-10-06 13:48:52 +0000

    Removing elements from last

--- LinkedList.java
+++ LinkedList.java
@@ -58,7 +58,28 @@ public class LinkedList {
         newNode.next = temp.next;
         temp.next = newNode;
     }
+    public int removeLast(){
+        if(size==0){
+            System.out.print("LinkedList is empty");
+            return -1;
+        }else if(size==1){
+            int val=head.data;
+            head=tail=null;
+            size=0;
+            return val;
 
+        }
+        Node prev=head;
+        for(int i=0;i<size-2;i++){
+            prev=prev.next;
+
+        }
+        int val=prev.next.data;
+        prev.next=null;
+        tail=prev;
+        size--;
+        return val;
+    }
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         ll.addFirst(2);
@@ -68,5 +89,9 @@ public class LinkedList {
         ll.add(2, 120);
         ll.print();
         System.out.println("Size of ll is " + ll.size);
+        ll.removeLast();
+        ll.print();
+        System.out.println("Size of ll is " + ll.size);
+
     }
 }
